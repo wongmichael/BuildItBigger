05833b11fea7ab0499d75e676a550e0ad08a265b
mike <mik9080@gmail.com>
2018-11-28 01:57:31 -0500

    1

---
+++ mylibrary/src/main/java/com/example/mylibrary/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.mylibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String JOKE_KEY = "joke";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"mylibrary mainAct onCreate",Toast.LENGTH_LONG).show();
+    }
+}
---
+++ mylibrary/src/main/java/com/example/mylibrary/MainFragment.java
@@ -0,0 +1,30 @@
+package com.example.mylibrary;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;;import static com.example.mylibrary.R.layout.fragment_main;
+
+public class MainFragment extends Fragment {
+    public MainFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //return
+        Toast.makeText(getActivity(),"mylibrary MainFrag onCreateView",Toast.LENGTH_LONG).show();
+        Log.d("mylibrary mainFragment","onCreateView");
+        View rootView = inflater.inflate(fragment_main,container,false);
+      /*  Intent intent = getActivity().getIntent();
+        String joke = intent.getStringExtra(MainActivity.JOKE_KEY);
+        TextView textView = rootView.findViewById(R.id.fragment_main_tv);
+        textView.setText(joke);*/
+        return rootView;
+    }
+}
